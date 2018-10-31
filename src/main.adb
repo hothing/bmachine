f7c8c64f00052b2904f9d340161ecb1501c25d32
Mykhayl Puzanov <mykhayl.puzanov@inteco-atec.at>
2018-10-31 16:07:20 +0100

    This try is god enough, but may be better.

--- src/main.adb
+++ src/main.adb
@@ -1,3 +1,4 @@
+with Ada.Text_IO; use Ada.Text_IO;
 procedure Main is
 
    type DataType is (TBool, TInt, TFloat);
@@ -75,6 +76,48 @@ procedure Main is
       return True;
    end ModInt;
 
+   function AndBool(a: PValuesArray) return Boolean is
+   begin
+      a.all(3).all.b := a.all(2).all.b and a.all(1).all.b;
+      return True;
+   end AndBool;
+
+   function OrBool(a: PValuesArray) return Boolean is
+   begin
+      a.all(3).all.b := a.all(2).all.b or a.all(1).all.b;
+      return True;
+   end OrBool;
+
+   function XorBool(a: PValuesArray) return Boolean is
+   begin
+      a.all(3).all.b := a.all(2).all.b xor a.all(1).all.b;
+      return True;
+   end XorBool;
+
+   function NotBool(a: PValuesArray) return Boolean is
+   begin
+      a.all(2).all.b := not a.all(1).all.b;
+      return True;
+   end NotBool;
+
+   function AssignIf(a: PValuesArray) return Boolean is
+   begin
+      if a.all(1).all.b then
+         a.all(3).all := a.all(2).all;
+      end if;
+      return True;
+   end AssignIf;
+
+   function Select2(a: PValuesArray) return Boolean is
+   begin
+      if a.all(1).all.b then
+         a.all(4).all := a.all(2).all;
+      else
+         a.all(4).all := a.all(3).all;
+      end if;
+      return True;
+   end Select2;
+
    function Stop(a: PValuesArray) return Boolean is
    begin
       return False;
@@ -104,25 +147,53 @@ begin
    pmem(7) := new Value(TInt);
    pmem(8) := new Value(TInt);
 
+   pmem(9) := new Value(TBool);
+   pmem(10) := new Value(TBool);
+   pmem(11) := new Value(TBool);
+   pmem(12) := new Value(TBool);
+   pmem(13) := new Value(TBool);
+   pmem(14) := new Value(TBool);
+
    ibs(1) := (AddInt'Access, new ValuesArray(1 .. 3));
-   ibs(1).args.all := (pmem(1), pmem(2), pmem(3));
+   ibs(1).args.all := (pmem(1), pmem(2), pmem(1));
+   pmem(1).all.i := 0;
+   pmem(2).all.i := 1;
 
    ibs(2) := (SubInt'Access, new ValuesArray(1 .. 3));
-   ibs(2).args.all := (pmem(3), pmem(4), pmem(5));
+   ibs(2).args.all := (pmem(1), pmem(3), pmem(4));
+   pmem(3).all.i := 1;
 
    ibs(3) := (MulInt'Access, new ValuesArray(1 .. 3));
    ibs(3).args.all := (pmem(5), pmem(7), pmem(6));
    pmem(7).all.i := 1;
    -- NOTE: ^ this is workaround for the raised Exception(ValueOverflow)
 
-   for i in 1 .. 10000000 loop
-      j := i mod 3 + 1;
+   ibs(4) := (NotBool'Access, new ValuesArray(1 .. 2));
+   ibs(4).args.all := (pmem(9), pmem(10));
+
+   ibs(5) := (XorBool'Access, new ValuesArray(1 .. 3));
+   ibs(5).args.all := (pmem(10), pmem(11), pmem(12));
+
+   ibs(6) := (OrBool'Access, new ValuesArray(1 .. 3));
+   ibs(6).args.all := (pmem(12), pmem(13), pmem(14));
+
+   ibs(7) := (AssignIf'Access, new ValuesArray(1 .. 3));
+   ibs(7).args.all := (pmem(12), pmem(4), pmem(6));
+
+   for i in 1 .. 100_000_000 loop
+      j := i mod 7 + 1;
       if ibs(j).func /= null then
          r := ibs(j).func(ibs(j).args);
       end if;
    end loop;
 
+   Put_Line(pmem(1).all.i'Image);
 end Main;
 
 -- elapsed time: 00.75s (mode:default; 1e7 instr)
 -- elapsed time: 00.44s (mode:optimize; 1e7 instr)
+-- elapsed time: 01.00s (mode:optimize; 1e8 instr)
+-- elapsed time: 03.10s (mode:default; 1e8 instr)
+
+-- this results may be good, but may improved
+-- if a descriminator check will be avoided
