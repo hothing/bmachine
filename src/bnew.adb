065bf083663388e897861f5141e7d0be6d0edb2f
Mykhayl Puzanov <barbos@inbox.ru>
2018-11-01 16:51:47 +0100

    A small reorganisation.
    * the constructors have been added
    * the aliased array as memory have been tested

--- src/bnew.adb
+++ src/bnew.adb
@@ -39,45 +39,82 @@ package body bnew is
      b.q1.pi.all := b.i1.pi.all - b.i2.pi.all;
    end DoCalc;
 
-   procedure Init (b : in out BlockSubI; i1, i2: PInt; q1 : PInt) is
+   procedure DoCalc (b : BlockMulI) is
    begin
-      b.i1.pi := i1;
-      b.i2.pi := i2;
-      b.q1.pi := q1;
-   end Init;
+     b.q1.pi.all := b.i1.pi.all * b.i2.pi.all;
+   end DoCalc;
+
+   procedure DoCalc (b : BlockDivI) is
+   begin
+     b.q1.pi.all := b.i1.pi.all / b.i2.pi.all;
+   end DoCalc;
+
+
+   --------------------------
+
+   package body Constructors is
+
+      function NewAddI (i1, i2: PInt; q1 : PInt) return PBlock is
+         b : PBlockAddI;
+      begin
+         b := new BlockAddI;
+         Init(b.all, i1, i2,q1);
+         return PBlock(b);
+      end NewAddI;
+
+
+      function NewSubI (i1, i2: PInt; q1 : PInt) return PBlock is
+         b : PBlockSubI;
+      begin
+         b := new BlockSubI;
+         Init(b.all, i1, i2,q1);
+         return PBlock(b);
+      end NewSubI;
+
+      function NewMulI (i1, i2: PInt; q1 : PInt) return PBlock is
+         b : PBlockMulI;
+      begin
+         b := new BlockMulI;
+         Init(b.all, i1, i2,q1);
+         return PBlock(b);
+      end NewMulI;
+
+      function NewDivI (i1, i2: PInt; q1 : PInt) return PBlock is
+         b : PBlockDivI;
+      begin
+         b := new BlockDivI;
+         Init(b.all, i1, i2,q1);
+         return PBlock(b);
+      end NewDivI;
+
+   end Constructors;
+
+
    --------------------------
 
    procedure DoTest is
-      a : PInt;
-      b : Pint;
-      c : PInt;
-      d : PInt;
+
+      mi : PMemInt := new MemInt(1 .. 10);
 
       bz : PBlock;
-      b1 : PBlockAddI;
-      b2 : PBlockSubI;
 
-      ibs : Instructions(1 .. 2);
+      ibs : Instructions(1 .. 4);
 
       j : Integer;
 
    begin
-      a := new Integer'(1);
-      b := new Integer'(2);
-      c := new Integer;
-      d := new Integer'(3);
 
-      -- ibs(1) := new BlockAddI (i1.pi => a, i2.pi => b, q1.pi => c);
-      -- ibs(2) := new BlockSubI (i1.pi => c, i2.pi => d, q1.pi => c);
+      mi(1) := 1;
+      mi(2) := 2;
+      mi(3) := 3;
+      mi(5) := 10; -- Integer'Last;
+      mi(6) := 5; -- Integer'First;
+      mi(8) := 10;
 
-      b1 := new BlockAddI;
-      b2 := new BlockSubI;
-
-      Init(b1.all, a, b ,c);
-      Init(b2.all, c, d ,c);
-
-      ibs(1) := PBlock(b1);
-      ibs(2) := PBlock(b2);
+      ibs(1) := Constructors.NewAddI(mi(1)'Access, mi(2)'Access, mi(4)'Access);
+      ibs(2) := Constructors.NewSubI(mi(4)'Access, mi(3)'Access, mi(4)'Access);
+      ibs(3) := Constructors.NewMulI(mi(5)'Access, mi(6)'Access, mi(7)'Access);
+      ibs(4) := Constructors.NewDivI(mi(7)'Access, mi(8)'Access, mi(9)'Access);
 
       j := ibs'First;
       for i in 1 .. 100_000_000 loop
@@ -87,7 +124,7 @@ package body bnew is
          j := j + 1; if j > ibs'Last then j := ibs'First; end if;
       end loop;
 
-      Put_Line(c.all'Image);
+      Put_Line(mi(4)'Image);
 
    end DoTest;
 
--- src/bnew.ads
+++ src/bnew.ads
@@ -20,6 +20,17 @@ package bnew is
    
    type PAny is access all RAny;
    
+   type MemBool is array(Positive range <>) of aliased Boolean;
+   type PMemBool is access MemBool;
+   
+   type MemInt is array(Positive range <>) of aliased Integer;
+   type PMemInt is access MemInt;
+   
+   type MemFloat is array(Positive range <>) of aliased Float;
+   type PMemFloat is access MemFloat;
+   
+   ---------------------------------------------------------------------------
+   
    type Block is abstract tagged null record;
    
    type PBlock is access all Block'Class;
@@ -40,16 +51,44 @@ package bnew is
    procedure DoCalc(b : BlockAddI);
    procedure Init (b : in out BlockAddI; i1, i2: PInt; q1 : PInt);
    
-   type BlockSubI is new Block with record
-      i1 : RAny(TInt);
-      i2 : RAny(TInt);
-      q1 : RAny(TInt);
-   end record;
+   type BlockSubI is new BlockAddI with null record;
    
    type PBlockSubI is access BlockSubI;
    
    procedure DoCalc(b : BlockSubI);
-   procedure Init (b : in out BlockSubI; i1, i2: PInt; q1 : PInt);
+   -- procedure Init (b : in out BlockSubI; i1, i2: PInt; q1 : PInt);
+   
+   
+   type BlockMulI is new BlockAddI with null record;
+   
+   type PBlockMulI is access BlockMulI;
+   
+   procedure DoCalc(b : BlockMulI);
+   -- procedure Init (b : in out BlockMulI; i1, i2: PInt; q1 : PInt);
+   
+   
+   type BlockDivI is new BlockAddI with null record;
+   
+   type PBlockDivI is access BlockDivI;
+   
+   procedure DoCalc(b : BlockDivI);
+   -- procedure Init (b : in out BlockDivI; i1, i2: PInt; q1 : PInt);
+   
+   -----------------
+   
+   package Constructors is
+      
+      function NewAddI (i1, i2: PInt; q1 : PInt) return PBlock;
+      
+      function NewSubI (i1, i2: PInt; q1 : PInt) return PBlock;
+      
+      function NewMulI (i1, i2: PInt; q1 : PInt) return PBlock;
+      
+      function NewDivI (i1, i2: PInt; q1 : PInt) return PBlock;
+      
+   end Constructors;
+   
+   -----------------
    
    procedure DoTest;   
    
